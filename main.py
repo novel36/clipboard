6b8d9aa0c35f20d695336b1e59bf6f789f63cceb
novel36 <novelwolde36@gmail.com>
2023-10-30 20:24:37 +0300

    .

---
+++ main.py
@@ -0,0 +1,25 @@
+import subprocess
+import time
+
+previous_clipboard_content = None
+
+def on_clipboard_update():
+  global previous_clipboard_content
+
+  # Get the new clipboard content.
+  clipboard_content = subprocess.check_output(["xclip", "-selection", "clipboard", "-o"], text=True)
+
+  # Only print the new clipboard content if it has changed.
+  if clipboard_content != previous_clipboard_content:
+    print("Clipboard content changed:", clipboard_content)
+
+    # Update the previous clipboard content.
+    previous_clipboard_content = clipboard_content
+
+# Start monitoring the clipboard.
+subprocess.Popen(["xclip", "-watch", "-selection", "clipboard"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+# Wait for the clipboard content to change.
+while True:
+  time.sleep(1)
+  on_clipboard_update()
